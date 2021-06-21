6a664082f65f03d7c502bb9667a6adc2c80413f8
dimidium <dim.khusnutdin@gmail.com>
2021-06-21 21:55:46 +0500

    add cod for clickLisener and show copter position

--- app/src/main/java/com/dimidium/lessonwork/MainActivity.java
+++ app/src/main/java/com/dimidium/lessonwork/MainActivity.java
@@ -2,29 +2,43 @@ package com.dimidium.lessonwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.icu.text.Transliterator;
 import android.os.Bundle;
 import android.view.View;
 import android.content.pm.ActivityInfo;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Integer positionNamber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        @SuppressLint("WrongViewCast") Button buttonUp = findViewById(R.id.goUp);
+        @SuppressLint("WrongViewCast") Button buttonDown = findViewById(R.id.goDown);
+        TextView position = findViewById(R.id.position);
 
-        findViewById(R.id.textClick).setOnClickListener(new View.OnClickListener() {
+        buttonUp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
+                positionNamber ++;
+            }
+      });
+        buttonDown.setOnClickListener(new View.OnClickListener() {
+           @Override
+            public void onClick(View v) {
+                positionNamber --;
             }
         });
-
-    }
-
+        position.setText(positionNamber);
+        }
 }
